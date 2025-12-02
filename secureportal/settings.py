a1a28f2b0acf6f31388c71d4dd1f785eedacdadd
sunitha-d63 <sunithagopal06@gmail.com>
2025-12-02 17:35:43 +0530

    new

--- secureportal/settings.py
+++ secureportal/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-key-for-dev")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+ALLOWED_HOSTS = ["fileportal.onrender.com", "localhost", "127.0.0.1"]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
